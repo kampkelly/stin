720cbbfd453d84dd9e944fda51de92f417aa036b
kampkelly <kampkellykeys@gmail.com>
2017-10-25 11:37:15 +0100

    mod analytics

--- resources/views/partials/_analytics.blade.php
+++ resources/views/partials/_analytics.blade.php
@@ -2,19 +2,19 @@
 	    <!-- Global site tag (gtag.js) - Google Analytics -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108601894-1"></script>
 		<script>
-		if (window.location.href.indexOf("theinnovestors.com") != -1) {
+	//	if (window.location.href.indexOf("theinnovestors.com") != -1) {
 		  window.dataLayer = window.dataLayer || [];
 		  function gtag(){dataLayer.push(arguments);}
 		  gtag('js', new Date());
 
 		  gtag('config', 'UA-108601894-1');
-		}
+	//	}
 		</script>
 
 		<!--piwik-->
 		<!-- Piwik -->
 	<script type="text/javascript">
-	if (window.location.href.indexOf("theinnovestors.com") != -1) {
+	//if (window.location.href.indexOf("theinnovestors.com") != -1) {
 	  var _paq = _paq || [];
 	  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 	  _paq.push(['trackPageView']);
@@ -26,6 +26,6 @@
 	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 	    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 	  })();
-	}
+//	}
 	</script>
 	<!-- End Piwik Code -->
