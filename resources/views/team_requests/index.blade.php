6e5f96affe73cbdb9dc0341c13dd249d4bb8b5be
kampkelly <kampkellykeys@gmail.com>
2017-11-16 19:37:46 +0100

    add tinymce editor for featured innovations

--- resources/views/team_requests/index.blade.php
+++ resources/views/team_requests/index.blade.php
@@ -2,12 +2,13 @@
 
 @section('content')
 
+<div style="background: #f2f2f2;">
 <div class="container docs">
 	<div style="height:45px;"></div>
      <h4 class="text-center" style="text-decoration: underline;">Team Requests </h4><br><br>
      @if(count($request_teams) >= 0)
          @foreach($request_teams as $request)
-         <div class="row" style="margin-bottom: 20px; background: #FAFAFA;">
+         <div class="row" style="margin-bottom: 20px; background: #f2f2f2;">
             <h5>{{$request->type}} request</h5>
              <span class="pull-right">{{$request->created_at->diffForHumans()}}</span> <span class="pull-right">on {{$request->category}}; &nbsp;</span>
               <a style="color:#133c57 !important; text-decoration: none;" href="/request_team/{{$request->slug}}">{{$request->user->fullname}} working on "{{$request->title}}" needs <span style="color:green;"> {{count($request->requestmembers)}} innovators</span> :<br>
@@ -28,5 +29,6 @@
            <h4>There are no teamup requests at this time!<br> You may submit one <a href="/request_team">here</a></h4>
       @endif
     <div style="height:200px;"></div>
+</div>
 </div>
 @endsection
