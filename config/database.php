e6b1eab1c2e1e8b2e0c5203e38eae25c5ed67425
kampkelly <kampkellykeys@gmail.com>
2018-03-07 14:50:52 +0100

    add heroku cred

--- config/database.php
+++ config/database.php
@@ -30,6 +30,12 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
 
     'connections' => [
 
@@ -41,11 +47,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host, '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database, 'forge'),
+            'username' => env('DB_USERNAME', $username, 'forge'),
+            'password' => env('DB_PASSWORD', $password, ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
