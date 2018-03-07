7c77a8d3e79b03802570168f04086f96f0766d3b
kampkelly <kampkellykeys@gmail.com>
2018-03-07 15:17:30 +0100

    add heroku cred

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
 return [
 
     /*
@@ -30,12 +35,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
+    
 
     'connections' => [
 
