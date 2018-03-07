da7a9d70dd516acde13cd5fcc5dd90804d844bec
kampkelly <kampkellykeys@gmail.com>
2018-03-07 17:00:08 +0100

    remove server lock

--- config/database.php
+++ config/database.php
@@ -63,11 +63,11 @@ return [
          # Our secondary database connection
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host, '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'piwik'),
-            'username' => env('DB_USERNAME', 'piwik'),
-            'password' => env('DB_PASSWORD', 'password'),
+            'database' => env('DB_DATABASE', $database, 'piwik'),
+            'username' => env('DB_USERNAME', $username, 'piwik'),
+            'password' => env('DB_PASSWORD', $password, 'password'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
